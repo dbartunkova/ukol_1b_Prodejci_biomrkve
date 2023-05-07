4d83fab4dc6d5bb015d42003b9fa51f3331c2918
Dana Bartunkova <DanaFicalova@gmail.com>
2023-05-07 09:34:37 +0200

    Prodejci mrkve - úkol č. 1b

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String args[]) {
+        String jmenoPrijmeni = "Dana Bartunkova";
+        //LocalDate datumNarozeni = 1990-03-16;
+        int pocetSmluv = 3;
+        double mnozstviMrkve = 50;
+        String mesto = "Ostrov nad Oslavou";
+        String SPZ = "5AV5643";
+        double spotreba = 5.5;
+        String IPadresa = "192.168.48.39";
+
+        System.out.println("Průměrný počet prodaných mrkví na jednu smlouvu je " + mnozstviMrkve/pocetSmluv + " tun. ");
+
+    }
+}
